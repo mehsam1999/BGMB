10dad746989547bbce704d9686561794510688e7
mehsam1999 <97000101+mehsam1999@users.noreply.github.com>
2022-01-10 20:42:51 +0600

    hello

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function show() {
     var email=document.getElementById("email").value;
     var message=document.getElementById("message").value;
 
-    firebase.database().ref(`User/${name}`).set({
+    firebase.database().ref('User/' + name).set({
         name : name,
         email : email,
         message : message,
@@ -16,3 +16,27 @@ function show() {
 
     });
 }
+
+  function search() {
+                          var name=document.getElementById("name1").value;
+
+                          firebase.database().ref('User/' + name).once('value').then(function(snapshot){
+                          if(snapshot.exists()) {
+                            var name_=snapshot.val().name;
+                            var mail_=snapshot.val().email;
+                            var message_=snapshot.val().message;
+                            document.getElementById("name").value=name_;
+                            document.getElementById("email").value=mail_;
+                            document.getElementById("message").value=message_;
+                          }
+                          else{
+
+                          }
+                        }, function(error) {
+                          if(error){
+
+                          } else{
+
+                          }
+                        });
+                            }
